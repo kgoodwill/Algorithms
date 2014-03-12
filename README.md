d32401f51edb3d02cce79ae64513f18c9ad012a7
Kyle Goodwill <kyle@debian>
2014-03-12 15:59:42 -0400

    SOOO MANY ERRORS

--- README.md
+++
@@ -1,1 +0,0 @@
-These are the algorithms and projects from CS375 - Design and Analysis of Algorithms at Binghamton University
--- longest_common_subsequence/program1.cpp
+++ longest_common_subsequence/program1.cpp
@@ -1,27 +1,45 @@
 //Program 1 - Bottom Up LCS Solution
 
-#include <stdio>
-#include <stdlib>
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <string.h>
+
+using namespace std;
 
 class BU_LCS
 {
 public:
 	BU_LCS();
 	~BU_LCS();
-	
 };
+
+int file_size(fstream file);
+
+int file_size(fstream file){
+	int count = 0;
+	string line;
+	while(!file.eof()){
+		line = file.getline();
+		count += line.length();
+	}
+	return count;
+}
 
 int main(int argc, char* argv){
 	if argc != 3{
-		cout << "Correct Syntax: program1 <input_file_1> <input_file_2> <output_file>"
+		cout << "Correct Syntax: program1 <input_file_1> <input_file_2> <output_file>" << endl;
 		return -1;
 	}
 	fstream inFile1;
 	fstream inFile2;
-	inFile1.open(argv[0]);
-	inFile2.open(argv[1]);
+	inFile1.open(argv[0], fstream::in);
+	inFile2.open(argv[1], fstream::in);
+
+	//Determine the size of each file
+	int size1 = file_size(inFile1);
+	int size2 = file_size(inFile2);
 
 	return 0;
 }
