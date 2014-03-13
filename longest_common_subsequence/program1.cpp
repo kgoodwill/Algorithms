8f18adba5f3917066fca34cdfea681b05ac0f6cc
Kyle Goodwill <k.kyle.goodwill@gmail.com>
2014-03-13 14:25:50 -0400

    almost there

--- longest_common_subsequence/program1.cpp
+++ longest_common_subsequence/program1.cpp
@@ -13,7 +13,7 @@ class LCS{
 		//Constructors & Destructors
 		LCS();
 		~LCS();
-		LCS(int rows, int columns, char* aIn, char* bIn);
+		LCS(int rows, int columns, int size1, int size2, char* aIn, char* bIn);
 
 		//Public Functions
 		void find_lcs();
@@ -25,6 +25,8 @@ class LCS{
 		//Data Members
 		int rows;
 		int columns;
+		int inputASize;
+		int inputBSize;
 		int** values;
 		int** directions;
 		char* inputA;
@@ -32,16 +34,16 @@ class LCS{
 };
 
 //Default Constructor
-LCS::LCS() : inputA(nullptr), inputB(nullptr), rows(1), columns(1){
-	LCS(rows, columns, inputA, inputB);
+LCS::LCS() : inputA(nullptr), inputB(nullptr), inputASize(1), inputBSize(1), rows(1), columns(1){
+	LCS(rows, columns, inputASize, inputBSize, inputA, inputB);
 }
 
 //Explicit Value Constructor
-LCS::LCS(int rows, int columns, char* aIn, char* bIn) : inputA(aIn), inputB(bIn), rows(rows), columns(columns){
+LCS::LCS(int rows, int columns, int inputSize1, int inputSize2, char* aIn, char* bIn) : inputA(aIn), inputB(bIn), inputASize(inputSize1), inputBSize(inputSize2), rows(rows), columns(columns){
 	values = new int*[rows];
 	for(int i=0; i<rows; i++){values[i] = new int[columns];}
-	directions = new int*[rows];
-	for(int i=0; i<rows; i++){directions[i] = new int[columns];}
+	directions = new int*[rows-1];
+	for(int i=1; i<rows; i++){directions[i] = new int[columns];}
 }
 
 LCS::~LCS(){
@@ -56,29 +58,48 @@ void LCS::find_lcs(){
 	for(int i=0; i<rows; i++){values[i][0] = 0;}
 	for(int i=0; i<columns; i++){values[0][i] = 0;}
 
-	for(int i=1; i<rows; i++){
-		for(int j=1; j<columns; j++){
-			if(inputA[i] == inputB[j]){
-				values[i][j] = (values[i-1][j-1]) + 1;
+	/*for(int i=0; i<rows; i++){cout << values[i][0] << " ";}
+	cout << endl;
+	for(int i=0; i<columns; i++){cout << values[0][i] << endl;}*/
+
+	for(int i=1; i<=inputASize; i++){
+		for(int j=1; j<=inputBSize; j++){
+			cout << "x[i]: " << inputA[i-1] << " " << i << endl;
+			cout << "y[j]: " << inputB[j-1] << " " << j << endl;
+			cout << endl;
+			if(inputA[i-1] == inputB[j-1]){
+				values[i][j] = (values[i-1][j-1] + 1);
 				directions[i][j] = 2;
+				cout << "Found Match: " << values[i][j] << endl;
+				cout << "Direction: " << directions[i][j] << endl;
 			}
 			else if(values[i-1][j] >= values[i][j-1]){
 				values[i][j] = values[i-1][j];
+				cout << values[i][j] << endl;
 				directions[i][j] = 3;
+				cout << "Direction: " << directions[i][j] << endl;
 			}
 			else{
 				values[i][j] = values[i][j-1];
+				cout << values[i][j] << endl;
 				directions[i][j] = 1;
+				cout << "Direction: " << directions[i][j] << endl;
 			}
 		}
 	}
 }
 
 void LCS::print_lcs(int i, int j){
+	cout << "i: " << i << endl;
+	cout << "j: " << j << endl;
 	if (i == 0 || j == 0){
 		exit(0);
 	}
+	//cout << directions[i][j] << endl;
 	if (directions[i][j] == 2){
+		//cout << "HERE" << endl;
+		//cout << directions[i][j] << endl;
+		cout << " " << inputA[i] << " ";
 		print_lcs(i-1, j-1);
 		cout << " " << inputA[i] << endl;
 	}
@@ -122,23 +143,25 @@ int main(int argc, char* argv[]){
 	inFile2 >> input2;
 	cout << input1 << endl;
 	cout << input2 << endl;
-	//inFile1.getline(input1, 1000);
-	//inFile2.getline(input2, 1000);
-	int size1 = input1.length() + 1;
-	int size2 = input2.length() + 1;
+	int inputALength = input1.length();
+	int inputBLength = input2.length();
+	cout << "x.length() - " << inputALength << endl;
+	cout << "y.length() - " << inputBLength << endl;	
+
+	int rowsize = inputALength + 1;
+	int colsize = inputBLength + 1;
+	cout << "rowsize - " << rowsize << endl;
+	cout << "colsize - " << colsize << endl;
 
 	char* cinput1 = (char*)input1.c_str();
 	char* cinput2 = (char*)input2.c_str();
-	//int size1 = input1.length();
-	cout << size1 << endl;
-	//int size2 = input2.length();
-	cout << size2 << endl;
 
 	//Create a LCS object
-	LCS* myLCS = new LCS(size1, size2, cinput1, cinput2);
+	LCS* myLCS = new LCS(rowsize, colsize, inputALength, inputBLength, cinput1, cinput2);
 
 	myLCS->find_lcs();
-	myLCS->print_lcs(size1, myLCS->get_Columns());
+	cout << "--LCS--" << endl;
+	myLCS->print_lcs(inputALength, inputBLength);
 
 	return 0;
 }
